2b01b892f8d9ded27a556137c0b3a3ac3305ae89
dbtkdok <dbtkdok@naver.com>
2024-02-29 18:04:28 +0900

    update file date

---
+++ koice_mvc/src/main/java/com/myWebShop/member/dao/FileDAOImpl.java
@@ -0,0 +1,23 @@
+package com.myWebShop.member.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.myWebShop.member.vo.FileVO;
+
+@Repository("fileDAO")
+public class FileDAOImpl implements FileDAO{
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public List<FileVO> file_list(FileVO files) throws DataAccessException {
+		List<FileVO> file_list= sqlSession.selectList("mapper.files.file_list", files);
+		return file_list;
+	}
+}
---
+++ koice_mvc/src/main/java/com/myWebShop/member/service/FileServiceImpl.java
@@ -0,0 +1,22 @@
+package com.myWebShop.member.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myWebShop.member.dao.FileDAO;
+import com.myWebShop.member.vo.FileVO;
+
+@Service("fileService")
+public class FileServiceImpl implements FileService{
+	
+	@Autowired
+	FileDAO fileDAO;
+
+	@Override
+	public List<FileVO> file_list(FileVO files) throws Exception {
+		// TODO Auto-generated method stub
+		return fileDAO.file_list(files);
+	}
+}
